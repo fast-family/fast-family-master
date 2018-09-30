827482c8d625c90f97ee55689bf255130610ac7c
ZShUn <616394599@qq.com>
2018-09-30 20:04:27 +0800

    no message

---
+++ fast-family-security/src/main/java/com/fast/family/security/validate/code/sms/SmsValidateCodeGenerator.java
@@ -0,0 +1,28 @@
+package com.fast.family.security.validate.code.sms;
+
+import com.fast.family.security.validate.code.ValidateCode;
+import com.fast.family.security.validate.code.ValidateCodeGenerator;
+import com.fast.family.utils.DateTimeUtils;
+import com.fast.family.utils.RandomUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 张顺
+ * @version 1.0
+ * @created 2018/9/23-23:19
+ */
+@Component
+public class SmsValidateCodeGenerator implements ValidateCodeGenerator {
+
+    @Autowired
+    private SmsValidateCodeProperties smsValidateCodeProperties;
+
+    @Override
+    public ValidateCode generate() {
+        String code = RandomUtils.genRandom(
+                smsValidateCodeProperties.getLength(),10);
+        return new ValidateCode(code, DateTimeUtils.convertLDTToLong(DateTimeUtils.parserDateTime()
+                .withMinute(smsValidateCodeProperties.getExpireIn())));
+    }
+}
