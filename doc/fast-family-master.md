98cb9bdad5c388bb62ba1efd8007a9292efd5feb
ZShUn <616394599@qq.com>
2018-10-13 00:30:20 +0800

    加入concurrent模块

---
+++ doc/fast-family-master.md
@@ -0,0 +1,16 @@
+[TOC]
+
+
+
+## fast-family-master
+
+###fast-family-mvc
+
+### fast-family-security
+
+### fast-family-datasource
+
+### fast-family-generator
+
+
+
---
+++ fast-family-concurrent/src/main/java/com/fast/family/concurrent/FastFamilyConcurrentApplication.java
@@ -0,0 +1,12 @@
+package com.fast.family.concurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFamilyConcurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastFamilyConcurrentApplication.class, args);
+    }
+}
