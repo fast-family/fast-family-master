86a6c6372439fbb0ecb4a4253bf25e0bb2199fb0
ZShUn <616394599@qq.com>
2018-10-15 09:46:56 +0800

    no message

---
+++ fast-family-mvc/src/main/java/com/fast/family/mvc/SpringMvcAutoConfigure.java
@@ -0,0 +1,10 @@
+package com.fast.family.mvc;
+
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties({SpringMvcProperties.class})
+public class SpringMvcAutoConfigure {
+}
