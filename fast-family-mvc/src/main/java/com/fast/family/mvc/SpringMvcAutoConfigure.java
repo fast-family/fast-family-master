d60514c4295032d38951794d051de8f4dff6f489
ZShUn <616394599@qq.com>
2018-10-23 23:00:53 +0800

    调整mvc 访问日志初始化方式

--- fast-family-mvc/src/main/java/com/fast/family/mvc/SpringMvcAutoConfigure.java
+++ fast-family-mvc/src/main/java/com/fast/family/mvc/SpringMvcAutoConfigure.java
@@ -1,10 +1,21 @@
 package com.fast.family.mvc;
 
 
+import com.fast.family.mvc.filter.AccessLogFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableConfigurationProperties({SpringMvcProperties.class})
 public class SpringMvcAutoConfigure {
+
+    @Bean
+    @ConditionalOnProperty(prefix = "fast.family.mvc.accessLog",name = "enabled",havingValue = "true")
+    public Filter accessLogFilter(){
+        return new AccessLogFilter();
+    }
 }
