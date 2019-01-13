d3ecf08e3b6816bee354a9f805e4002fd7b2769d
ZShUn <616394599@qq.com>
2019-01-13 22:27:06 +0800

    加入通用返回结果类

---
+++ fast-family-commons/src/main/java/com/fast/family/commons/json/Response.java
@@ -0,0 +1,45 @@
+package com.fast.family.commons.json;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author 张顺
+ * @version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response<T> {
+
+    private String message;
+
+    private int code;
+
+    private T data;
+
+
+    public static <T>Response<T> ok(T data){
+        return ok(200,"success",data);
+    }
+
+    public static <T>Response<T> ok(int code,String message,T data){
+        Response response = new Response();
+        response.setCode(code);
+        response.setData(data);
+        response.setMessage(message);
+        return response;
+    }
+
+    public static <T>Response<T> fail(){
+        return fail(500,"runtimeException");
+    }
+
+    public static <T>Response<T> fail(int code,String message){
+        Response response = new Response();
+        response.setCode(code);
+        response.setMessage(message);
+        return response;
+    }
+}
