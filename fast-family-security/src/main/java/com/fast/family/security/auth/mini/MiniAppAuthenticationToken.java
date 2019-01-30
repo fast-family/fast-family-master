c80108a0dd22ffaa9a4fa1da295aa10b4aee0945
616394599@qq.com <616394599@qq.com>
2019-01-30 18:05:03 +0800

    提交一丢丢代码

--- fast-family-security/src/main/java/com/fast/family/security/auth/mini/MiniAppAuthenticationToken.java
+++ fast-family-security/src/main/java/com/fast/family/security/auth/mini/MiniAppAuthenticationToken.java
@@ -10,14 +10,25 @@ import java.util.Collection;
  * @version 1.0
  */
 public class MiniAppAuthenticationToken extends BaseAuthenticationToken {
+
+
+    private final Object principal;
+
+    public MiniAppAuthenticationToken(Object principal){
+        super(null);
+        this.principal = principal;
+    }
+
     /**
      * Creates a token with the supplied array of authorities.
      *
      * @param authorities the collection of <tt>GrantedAuthority</tt>s for the principal
      *                    represented by this authentication object.
+     * @param principal
      */
-    public MiniAppAuthenticationToken(Collection<? extends GrantedAuthority> authorities) {
+    public MiniAppAuthenticationToken(Collection<? extends GrantedAuthority> authorities, Object principal) {
         super(authorities);
+        this.principal = principal;
     }
 
     @Override
